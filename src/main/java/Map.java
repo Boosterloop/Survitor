bcd139c056dc450c9082e42129ad8147c232cb94
mgirard92@gmail.com <mgirard92@gmail.com>
2019-05-22 16:59:46 +0200

    Brouillon de collisions + timer

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -1,2 +1,15 @@
 public class Map {
+    private int timeout;
+
+    public Map(int timeout) {
+        this.timeout = timeout;
+    }
+
+    public int getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(int timeout) {
+        this.timeout = timeout;
+    }
 }
